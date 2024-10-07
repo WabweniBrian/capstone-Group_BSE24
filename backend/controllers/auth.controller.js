d361b551210f20c405370837bf4d8d84f2208052
WabweniBrian <wabzbraize66@gmail.com>
2024-10-07 09:46:53 +0300

    Updates

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -5,15 +5,15 @@ import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   res.status(201).json({ message: "User created successfully" });
-  // const { username, email, password } = req.body;
-  // const hashedPassword = bcryptjs.hashSync(password, 10);
-  // const newUser = new User({ username, email, password: hashedPassword });
-  // try {
-  //   await newUser.save();
-  //   res.status(201).json({ message: "User created successfully" });
-  // } catch (error) {
-  //   next(error);
-  // }
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const signin = async (req, res, next) => {
