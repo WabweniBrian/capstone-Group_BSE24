c4e3fba8c7fef8b9049a35f60e85839b248800cc
WabweniBrian <wabzbraize66@gmail.com>
2024-10-07 09:13:47 +0300

    Updates

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 app.use(express.json());
